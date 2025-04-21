e33668e1763866a6d6965a3ef581ef036c5f7011
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-21 09:45:21 -0500

    Primera instancia del menú

---
+++ library/menu/menu.py
@@ -0,0 +1,80 @@
+from datetime import date
+from library.items.book import Book
+from library.menu.book_menu import handle_books_management
+from library.menu.category_menu import handle_categories_management
+from library.people.author import Author
+from library.categories.category import Category
+from library.helpers.item_status import ItemStatus
+
+
+def display_menu():
+    while True:
+        print("\n======= SISTEMA DE GESTIÓN DE BIBLIOTECA =======\n")
+        print("1. Gestión de libros")
+        print("2. Gestión de artículos científicos")
+        print("3. Gestión de tesis")
+        print("4. Gestión de categorías")
+        print("5. Gestión de copias de libros")
+        print("6. Gestión de autores")
+        print("7. Gestión de lectores")
+        print("8. Gestión de préstamos")
+        print("9. Gestión de multas")
+        print("0. Salir\n")
+
+        try:
+            option = int(input("Seleccione una opción: "))
+
+            if option == 0:
+                print("\nGracias por usar el sistema. ¡Hasta pronto!")
+                break
+            elif option == 1:
+                handle_books_management()
+            elif option == 2:
+                handle_articles_management()
+            elif option == 3:
+                handle_thesis_management()
+            elif option == 4:
+                handle_categories_management()
+            elif option == 5:
+                handle_copies_management()
+            elif option == 6:
+                handle_authors_management()
+            elif option == 7:
+                handle_readers_management()
+            elif option == 8:
+                handle_loans_management()
+            elif option == 9:
+                handle_fines_management()
+            else:
+                print("\nOpción inválida. Por favor, intente nuevamente.")
+
+        except ValueError:
+            print("\nError: Por favor ingrese un número válido.")
+
+
+def handle_articles_management():
+    print("\nGestión de artículos científicos - En desarrollo...")
+
+
+def handle_thesis_management():
+    print("\nGestión de tesis - En desarrollo...")
+
+
+def handle_copies_management():
+    print("\nGestión de copias de libros - En desarrollo...")
+
+
+def handle_authors_management():
+    print("\nGestión de autores - En desarrollo...")
+
+
+def handle_readers_management():
+    print("\nGestión de lectores - En desarrollo...")
+
+
+def handle_loans_management():
+    print("\nGestión de préstamos - En desarrollo...")
+
+
+def handle_fines_management():
+    print("\nGestión de multas - En desarrollo...")
