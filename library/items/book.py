3f5d1b7f90f2a28becce01b624a384b88c0b34dc
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-21 10:53:34 -0500

    Correción de tipados

--- library/items/book.py
+++ library/items/book.py
@@ -4,9 +4,9 @@ from typing import List
 from datetime import date
 from library.categories.category import Category
 from library.copies.copies import Copies
+from library.helpers.copy_status import Copy_status
 from library.helpers.item_status import ItemStatus
 from library.people.author import Author
-
 
 
 class Book:
@@ -40,8 +40,8 @@ class Book:
     def __str__(self) -> str:
         authors_str = "\n".join(str(author) for author in self.authors)
         categories_str = "\n".join(str(category) for category in self.categories)
-        copias=Copies.get_copy_by_ISBN(self.ISBN)
-        available_copies=len(copias)
+        copias = Copies.get_copy_by_ISBN(self.ISBN)
+        available_copies = len(copias)
 
         return (
             f"Título:{self.title}\n"
@@ -89,63 +89,61 @@ class Book:
         )
         cls._books.append(new_book)
 
-        #Debe registrarse al menos 1 copia
+        # Debe registrarse al menos 1 copia
         Copies.register(
-            copy_ID=f"Copia_{ISBN}_1",
-            ISBN=ISBN,
-            status=ItemStatus.AVAILABLE
+            copy_ID=f"Copia_{ISBN}_1", ISBN=ISBN, status=Copy_status.IN_LIBRARY
         )
 
         return True
 
     @classmethod
-    def get_book_by_ISBN(cls, 
-                         ISBN: str,
-                         ) -> Book | None:
+    def get_book_by_ISBN(
+        cls,
+        ISBN: str,
+    ) -> Book | None:
         for book in cls._books:
             if book.ISBN == ISBN:
                 return book
         return None
+
     @classmethod
     def get_books(cls) -> List[Book]:
         return cls._books
 
     def update_book(
-            self,
-            new_genre:str=None,
-            new_title:str=None,
-            new_edition:str=None,
-            new_publication_date:date=None,
-            new_publisher:str=None,
-            new_authors:List[Author]=None,
-            new_language:str=None,
-            new_status:ItemStatus=None,
-            new_categories:List[Category]=None,
-    )-> None:
-         
-         if self.status != ItemStatus.AVAILABLE:
-           print("No se puede modificar un libro inhabilitado.")
-           return
-         if new_genre:
-            self.genre=new_genre
-         if new_title:
-            self.title=new_title
-         if new_edition:
-            self.edition=new_edition
-         if new_publication_date:
-            self.publication_date=new_publication_date
-         if new_publisher:
-            self.publisher=new_publisher
-         if new_authors:
-            self.authors=new_authors  
-         if new_language:
-            self.language=new_language
-         if new_status:
-            self.status=new_status
-         if new_categories:
-            self.categories=new_categories
-  
-    def disable_book(self)->None:
-        self.status=ItemStatus.UNAVAILABLE
+        self,
+        new_genre: str | None = None,
+        new_title: str | None = None,
+        new_edition: str | None = None,
+        new_publication_date: date | None = None,
+        new_publisher: str | None = None,
+        new_authors: List[Author] | None = None,
+        new_language: str | None = None,
+        new_status: ItemStatus | None = None,
+        new_categories: List[Category] | None = None,
+    ) -> None:
 
+        if self.status != ItemStatus.AVAILABLE:
+            print("No se puede modificar un libro inhabilitado.")
+            return
+        if new_genre:
+            self.genre = new_genre
+        if new_title:
+            self.title = new_title
+        if new_edition:
+            self.edition = new_edition
+        if new_publication_date:
+            self.publication_date = new_publication_date
+        if new_publisher:
+            self.publisher = new_publisher
+        if new_authors:
+            self.authors = new_authors
+        if new_language:
+            self.language = new_language
+        if new_status:
+            self.status = new_status
+        if new_categories:
+            self.categories = new_categories
 
+    def disable_book(self) -> None:
+        self.status = ItemStatus.UNAVAILABLE
